9340f8d795ead9c9caad09ae23727eed48c6ac43
Berkay Ucar <berkayucar80@gmail.com>
2024-12-06 21:17:43 +0300

    Accessing query string parameters

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,17 @@
+import { log } from "console";
 import UserTable from "./UserTable";
+import { sort } from "fast-sort";
 
-const UsersPage = async () => {
+interface Props {
+  searchParams: { sortOrder: string };
+}
+
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
       {/* <p>{new Date().toLocaleTimeString()}</p> */}
-      <UserTable />
+      <UserTable sortOrder={sortOrder} />
     </>
   );
 };

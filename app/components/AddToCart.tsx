211ed16179d37b1a3ee0826c9b249860895dd70a
Berkay Ucar <berkayucar80@gmail.com>
2024-12-06 10:24:11 +0300

    next.js initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { log } from 'console';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Click')}>Add to Cart</button>
+    </div>
+  )
+}
+
+export default AddToCart

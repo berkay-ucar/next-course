779232b12e74a4fc03a099a86e0cf2f5085394c1
Berkay Ucar <berkayucar80@gmail.com>
2024-12-09 12:21:06 +0300

    building APIs POST body

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(
-  request: NextRequest,
-  { params }: { params: { id: number } }
-) {
+export async function GET(request: NextRequest, props: { params: Promise<{ id: number }> }) {
+  const params = await props.params;
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Mosh" });

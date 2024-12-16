2e26fe8c6ab9dc294dec182ebbd9c7b45423efa9
Berkay Ucar <berkayucar80@gmail.com>
2024-12-16 22:47:57 +0300

    Accessing sessions on the client

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./Navbar";
+import AuthProvider from "./auth/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <main className="p-5">
-          {/* <Suspense fallback={<p>Loading..</p>}>{children}</Suspense> */}
-          {children}
-        </main>
+        <AuthProvider>
+          <Navbar />
+          <main className="p-5">
+            {/* <Suspense fallback={<p>Loading..</p>}>{children}</Suspense> */}
+            {children}
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
---
+++ app/upload/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="A coffee image"
+        />
+      )}
+
+      <CldUploadWidget
+        uploadPreset="ml_default"
+        //options={{ sources: ["local"] }}
+        onSuccess={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;

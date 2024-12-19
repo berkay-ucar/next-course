37cd3abd5eea5d2681a83789d76da6e5162a106e
Berkay Ucar <berkayucar80@gmail.com>
2024-12-19 10:40:00 +0300

    optimising images 1

---
+++ app/api/send-email/route.tsx
@@ -0,0 +1,15 @@
+import WelcomeTemplate from "@/emails/WelcomeTemplate";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  await resend.emails.send({
+    from: "you@example.com",
+    to: "user@gmail.com",
+    subject: "hello world",
+    react: <WelcomeTemplate name="Mosh" />,
+  });
+  return NextResponse.json({});
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import coffee from "@/public/images/coffee.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
+  // return (
+  //   <main>
+  //     <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+  //     <Link href="/users">Users</Link>
+  //     <ProductCard />
+  //   </main>
+  // );
+
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <Image src={coffee} alt="coffee"></Image>
     </main>
   );
 }

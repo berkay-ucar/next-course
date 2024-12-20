a12621fba1785422db1214a9cc0ea8638a06ab33
Berkay Ucar <berkayucar80@gmail.com>
2024-12-20 10:36:54 +0300

    Lazy Loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import coffee from "@/public/images/coffee.jpg";
+import HeavyComponent from "./components/HeavyComponent";
 
-export default async function Home() {
+export default function Home() {
   // const session = await getServerSession(authOptions);
   // return (
   //   <main>
@@ -18,6 +21,7 @@ export default async function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <HeavyComponent />
       <main className="relative h-screen">
         {/* <Image src={coffee} alt="coffee"></Image> */}
         {/* <Image
